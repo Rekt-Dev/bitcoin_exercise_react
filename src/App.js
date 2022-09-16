f70225d252d6773c96eb1af51b2de7f5f8a2191e
rektDev <rektdevvv@gmail.com>
2022-09-16 16:44:06 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import {useState} from "react"
 
 export default function App() {
   
-  const [price,setPrice]=useState(0)
+  const [price,setPrice]=useState([])
+  const [pressed,setPressed]=useState(false)
   
   async function checkBtcPrice() {
     const response = await fetch(
-`https://api.coingecko.com/api/v3/simple/price?no-cors&ids=bitcoin&vs_currencies=usd&no-cors&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false
+`https://api.coingecko.com/api/v3/simple/price/?&no-cors&ids=bitcoin&vs_currencies=usd&no-cors&include_market_cap=false&include_24hr_vol=false&include_24hr_change=false&include_last_updated_at=false
 `);
     setPrice(await response.json());
     console.log(price);
   }
     
-  const fakePrice=()=>{
-    setPrice(18777)
-  }
+
   
   return (
     <div className="App">
